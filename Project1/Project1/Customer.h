f7ca8856d75fac1f823770483f25452c4372cba1
Stoian Cristiana <stoiancristiana21@stud.ase.ro>
2022-12-06 22:29:01 +0200

    added customer .h

--- Project1/Project1/Customer.h
+++ Project1/Project1/Customer.h
@@ -1,1 +1,62 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Customer
+{
+private:
+	string full_name;
+	int no_tickets_bought;
+	int card;
+public:
+	//Default Constructor
+	Customer()
+	{
+		this->full_name = "default_name";
+		this->no_tickets_bought=0;
+		this->card = card=0;
+	}
+	//Constructor with parameters
+	Customer(string name, int no_tickets_bought,int card)
+	{
+		this->full_name = full_name;
+		this->no_tickets_bought = no_tickets_bought;
+		this->card = card;
+	}
+	//setter
+	void Setfull_name( string full_name)
+	{
+		this->full_name = full_name;	}
+	void Setno_tickets_bought(int no_tickets_bought)
+	{
+		this->no_tickets_bought = no_tickets_bought;
+
+	}
+	void Setcard(int card)
+	{
+		this->card = card;
+	}
+	//GETTERS
+	string Getfull_name(string full_name)
+	{
+		return full_name;
+	}
+	int Getno_tickets_bought(int no_tickets_bought)
+	{
+		return no_tickets_bought;
+	}
+	int Getcard(int card)
+
+	{
+		return card;
+	}
+	//Operators
+	friend istream& operator >>(istream& in, Customer& c);
+
+	friend ostream& operator <<(ostream& out, const Customer& c);
+
+};
+istream& operator>> (istream& in, Customer& c) {
+	in >> c.full_name>>c.no_tickets_bought >> c.card;
+	return in;
+}ostream& operator<<(ostream& out, const Customer& c)
--- Project1/Project1/Ticket.h
+++ Project1/Project1/Ticket.h
@@ -31,4 +31,64 @@ public:
 		this->typeOfTicket = typeOfTicket;
 		this->id = id;
 	}
+	//setters
+	void Setmovie_details(string movie_details)
+	{
+		this->movie_details = movie_details;
+
+	}
+	void Sethall_no(int hall_no)
+	{
+		this->hall_no = hall_no;
+	}
+	void Setrow(int row)
+	{
+		this->row = row;
+	}
+	void Setseat(int seat)
+	{
+		this->seat = seat;
+	}
+	void SettypeOfTicket(int typeOfTicket)
+	{
+		this->typeOfTicket = typeOfTicket;
+	}
+	void Setid()
+	{
+		this->id = id;
+	}
+	//getters
+	string Getmovie_details(string movie_details)
+	{
+		return movie_details;
+ }
+	int Gethall_no(int hall_no)
+	{
+		return hall_no;
+	}
+	int Getrow()
+	{
+		return row;
+	}
+	int Getseat() {
+
+		return seat;
+	}
+	int GettypeOfTicket(int typeOfTicket)
+	{
+		return typeOfTicket;
+	}
+	int Getid(int id)
+	{
+		return id;
+	}
+	//Operators
+	friend istream& operator >>(istream& in, Ticket& t);
+
+	friend ostream& operator <<(ostream& out, const Ticket& t);
 };
+istream& operator>> (istream& in, Ticket& t) {
+	in >>t.movie_details >> t.hall_no >> t.row >> t.seat>> t.typeOfTicket>> t.id;
+	return in;
+}
+ostream& operator<<(ostream& out, const Ticket& t)
